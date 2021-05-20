e89d12ae460d0a99c1264c5ede61e91c495cf936
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-05-21 01:00:19 +0500

    Update book.py

--- tg_bot/modules/book.py
+++ tg_bot/modules/book.py
@@ -72,7 +72,7 @@ async def _(event):
         await client.send_file(
             event.chat_id,
             "book.txt",
-            caption=f"**BOOKS GATHERED SUCCESSFULLY!\n\nBY DAISYX. JOIN THE SUPPORT @DaisySupport_Official.**",
+            caption=f"**BOOKS GATHERED SUCCESSFULLY!\n\nBY ELIZA. JOIN THE SUPPORT @eagleunionelizasupport.**",
         )
         os.remove("book.txt")
         await KkK.delete()
