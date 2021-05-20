a65cfaf759b643eb0dcd5c7a764e99315563a03c
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-05-21 03:06:14 +0500

    Create elizabot.py

---
+++ tg_bot/elizabot.py
@@ -0,0 +1,30 @@
+from tg_bot import client
+from telethon import events
+
+
+def eliza(**args):
+    """New message."""
+    pattern = args.get('pattern', None)
+    r_pattern = r'^[/!]'
+    if pattern is not None and not pattern.startswith('(?i)'):
+        args['pattern'] = '(?i)' + pattern
+    args['pattern'] = pattern.replace('^/', r_pattern, 1)
+
+    def decorator(func):
+        telethn.add_event_handler(func, events.NewMessage(**args))
+        return func
+
+    return decorator
+
+
+def inlinequery(**args):
+    """Inline query."""
+    pattern = args.get('pattern', None)
+    if pattern is not None and not pattern.startswith('(?i)'):
+        args['pattern'] = '(?i)' + pattern
+
+    def decorator(func):
+        telethn.add_event_handler(func, events.InlineQuery(**args))
+        return func
+
+    return decorator
