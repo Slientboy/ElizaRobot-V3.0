a70c8ec705951707f3e0413e62f72c94053e8e47
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-05-23 01:51:42 +0500

    Update cinfo.py

--- tg_bot/modules/cinfo.py
+++ tg_bot/modules/cinfo.py
@@ -2,7 +2,7 @@ import html
 from pyrogram import Client, filters
 from pyrogram.errors.exceptions.bad_request_400 import MessageNotModified
 
-from tg_bot import kp, get_entity
+from tg_bot import pbot
 
 ZWS = "\u200B"
 
@@ -31,7 +31,7 @@ def _generate_sexy(entity, ping):
     return sexy_text
 
 
-@kp.on_message(filters.command(["cinfo"], prefixes=["/", "!"]))
+@pbot.on_message(filters.command(["cinfo"], prefixes=["/", "!"]))
 async def info(client, message):
     entity = message.chat
     command = message.command
@@ -72,7 +72,7 @@ async def info(client, message):
             pass
 
 
-@kp.on_message(filters.command(["getid"], prefixes=["/", "!"]))
+@pbot.on_message(filters.command(["getid"], prefixes=["/", "!"]))
 async def id(client, message):
     text_unping = "<b>Chat ID:</b>"
     if message.chat.username:
