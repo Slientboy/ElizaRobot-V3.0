5b09113ea0ce0ad27e0985ca25c6b084cc29efb0
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-06-01 22:10:15 +0500

    Update fetch.py

--- tg_bot/utils/fetch.py
+++ tg_bot/utils/fetch.py
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-from tg_bot import aiohtt
+from tg_bot import aiohttp
 
 headers = {
     "Accept": "application/json",
