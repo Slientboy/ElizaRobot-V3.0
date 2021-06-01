008f31b4de98874e3c25293cde4e92b08bee0d86
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-06-01 22:15:22 +0500

    Update fetch.py

--- tg_bot/utils/fetch.py
+++ tg_bot/utils/fetch.py
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-from tg_bot import aiohttp
+from tg_bot import aiohttpsession
 
 headers = {
     "Accept": "application/json",
@@ -30,7 +30,7 @@ headers = {
 
 
 async def fetch(url: str):
-    async with aiohtt.get(url, headers=headers) as resp:
+    async with aiohttpsession.get(url, headers=headers) as resp:
         try:
             data = await resp.json()
         except Exception:
