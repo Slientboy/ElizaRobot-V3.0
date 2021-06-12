6571ef5ba3f0e83e2d261bec2a5e243fbb0a4b46
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-06-13 01:52:09 +0500

    Update mongo.py

--- tg_bot/utils/mongo.py
+++ tg_bot/utils/mongo.py
@@ -1,4 +1,4 @@
-#Kiitu
+#make by eagle union
 import asyncio
 import sys
 
@@ -17,7 +17,7 @@ MONGO_DB = "DaisyX"
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
-db = motor[MONGO_DB_URL]
+db = motor[MONGO_DB]
 db = client["yonerobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
