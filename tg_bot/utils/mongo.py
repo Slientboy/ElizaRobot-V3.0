5271a5ba3532cb635de0860ebfb076bddcf5a574
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-06-13 02:09:47 +0500

    Update mongo.py

--- tg_bot/utils/mongo.py
+++ tg_bot/utils/mongo.py
@@ -22,4 +22,4 @@ db = client["yonerobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
-    sys.exit(log.critical("Can't connect to mongodb! Exiting..."))
+    sys.exit(loggingcritical("Can't connect to mongodb! Exiting..."))
