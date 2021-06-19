575574a660d7298e7f4548b59c5199c46ef2eae7
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-06-19 20:09:29 +0500

    Update logomaker.py

--- tg_bot/modules/logomaker.py
+++ tg_bot/modules/logomaker.py
@@ -26,7 +26,7 @@ async def lego(event):
     pointsize = 500
     fillcolor = "gold"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./YoneRobot/resource/Chopsic.otf", 330)
+    font = ImageFont.truetype("./tg_bot/resource/Chopsic.otf", 330)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
@@ -60,13 +60,13 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./tg_bot/blackbg.jpg')
+    img = Image.open('./tg_bot/resource/blackbg.jpg')
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
     fillcolor = "white"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./YoneRobot/resource/Maghrib.ttf", 1000)
+    font = ImageFont.truetype("./tg_bot/resource/Maghrib.ttf", 1000)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
@@ -74,7 +74,7 @@ async def lego(event):
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
     draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
-    fname2 = "LogoByYone.png"
+    fname2 = "LogoByRam.png"
     img.save(fname2, "png")
     await tbot.send_file(event.chat_id, fname2, caption="Made By YoneRobot")
     if os.path.exists(fname2):
