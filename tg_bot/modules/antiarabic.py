c2cfbb14cd259b1ce226be9866b102c391a7e75c
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-06-19 19:10:58 +0500

    Update antiarabic.py

--- tg_bot/modules/antiarabic.py
+++ tg_bot/modules/antiarabic.py
@@ -2,7 +2,7 @@
 from typing import List
 
 from telegram import Update, Bot, ParseMode
-from telegram.ext import CommandHandler, MessageHandler, Filters
+from telegram.ext import CallbackContext, CommandHandler, MessageHandler, Filters
 from telegram.ext.dispatcher import run_async
 
 from tg_bot import dispatcher
@@ -10,13 +10,15 @@ from tg_bot.modules.helper_funcs.chat_status import user_not_admin, user_admin, 
 from tg_bot.modules.helper_funcs.extraction import extract_text
 from tg_bot.modules.sql import antiArabic_sql as sql
 
-
 ANTIARABIC_GROUPS = 12
 
 
 @run_async
 @user_admin
-def antiarabic_setting(bot: Bot, update: Update, args: List[str]):
+def antiarabic_setting(update: Update, context: CallbackContext) -> str:
+    bot = context.bot
+    args = context.args
+
     chat = update.effective_chat
     msg = update.effective_message
     user = update.effective_user
@@ -26,20 +28,23 @@ def antiarabic_setting(bot: Bot, update: Update, args: List[str]):
         if len(args) >= 1:
             if args[0].lower() in ("yes", "on", "true"):
                 sql.set_chat_setting(chat.id, True)
-                msg.reply_text("antiarabic_enabled")
+                msg.reply_text("Turned on AntiArabic! Messages sent by any non-admin which contains arabic text will be deleted.")
 
             elif args[0].lower() in ("no", "off", "false"):
                 sql.set_chat_setting(chat.id, False)
-                msg.reply_text(tld(chat.id, "antiarabic_disabled"))
+                msg.reply_text("Turned off AntiArabic! Messages containing arabic text won't be deleted.")
         else:
-            msg.reply_text("antiarabic_setting").format(
+           msg.reply_text("</antiarabic on/off> to turn on or turn off AntiArabic Mode.").format(
                 sql.chat_antiarabic(chat.id),
                 parse_mode=ParseMode.MARKDOWN)
 
 
 @user_not_admin
 @run_async
-def antiarabic(bot: Bot, update: Update):
+def antiarabic(update: Update, context: CallbackContext):
+    bot = context.bot
+    args = context.args
+
     chat = update.effective_chat
     msg = update.effective_message
     to_match = extract_text(msg)
@@ -74,10 +79,12 @@ def __migrate__(old_chat_id, new_chat_id):
     sql.migrate_chat(old_chat_id, new_chat_id)
 
 
-__help__= """
+
+
+
+__help__ = """
 \
 Here is some help for the AntiArabicScript module:
-
 AntiArabicScript module is used to delete messages containing characters from one of the following automatically:
 
 • Arabic
@@ -86,16 +93,15 @@ AntiArabicScript module is used to delete messages containing characters from on
 • Arabic Presentation Forms-A
 • Arabic Presentation Forms-B
 • Rumi Numeral Symbols
-• Arabic Mathematical Alphabetic Symbols
+• Arabic Mathematical Alphabetic Symbol
 
 NOTE: AntiArabicScript module doesn't affect messages sent by admins.
 
 Admin only:
- - /antiarabic <on/off>: turn antiarabic module on/off ( off by default )
- - /antiarabic: get status of AntiArabicScript module in chat.
+ - `/antiarabic` `<on/off>`*:* turn antiarabic module on/off ( off by default )
 \
 """
-__mod_name__ = "AntiArabicScript"
+__mod_name__ = "Anti Arabic"
 
 SETTING_HANDLER = CommandHandler("antiarabic", antiarabic_setting,
                                  pass_args=True)
