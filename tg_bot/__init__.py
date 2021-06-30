05fe498ba46211d61e06540be450cc502da27f78
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-06-30 23:57:35 +0500

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -74,6 +74,7 @@ if ENV:
     PORT = int(os.environ.get('PORT', 5000))
     CERT_PATH = os.environ.get("CERT_PATH")
 
+    GBAN_DUMP = os.environ.get("GBAN_DUMP", None)
     API_ID = os.environ.get('API_ID', None)
     API_HASH = os.environ.get('API_HASH', None)
     STRING_SESSION = os.environ.get('STRING_SESSION', None)
