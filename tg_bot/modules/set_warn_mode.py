1bb4563707397aab75c583acb60a4aaba8b46634
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-07-17 00:51:59 +0500

    Update set_warn_mode.py

--- tg_bot/modules/set_warn_mode.py
+++ tg_bot/modules/set_warn_mode.py
@@ -18,12 +18,12 @@
 import html
 from enum import Enum, auto
 
-from Stella import StellaCli
-from Stella.database.warnings_mongo import set_warn_mode_db
-from Stella.helper import custom_filter
-from Stella.helper.chat_status import isBotAdmin, isUserCan
-from Stella.helper.convert import convert_time
-from Stella.helper.time_checker import get_time, time_string_helper
+from tg_bot import pbot as StellaCli
+from tg_bot.database.warnings_mongo import set_warn_mode_db
+from tg_bot.helper import custom_filter
+from tg_bot.helper.chat_status import isBotAdmin, isUserCan
+from tg_bot.helper.convert import convert_time
+from tg_bot.helper.time_checker import get_time, time_string_helper
 
 
 class WarnModeMap(Enum):
